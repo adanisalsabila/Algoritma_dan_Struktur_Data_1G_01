611f4d910afbf5144322c9ded3a81bc4f12f704e
adanisalsabilaaa@gmail.com <adanisalsabilaaa@gmail.com>
2024-02-20 09:00:00 +0700

    Jobsheet2_Percobaan3_01_1G

--- src/P2/Buku01.java
+++ src/P2/Buku01.java
@@ -1,34 +1,45 @@
 package P2;
+
 public class Buku01 {
+    public Buku01(){
+    }
+
+public Buku01 (String jud, String pg, int hal, int stok, int har){
+    judul = jud;
+    pengarang = pg;
+    halaman=hal;
+    this.stok=stok;
+    harga=har;
+}
+
     String judul, pengarang;
     int halaman, stok, harga;
 
-
     void tampilInformasi() {
-    System.out.println("Judul : "+judul);
-    System.out.println("Pengarang : "+pengarang);
-    System.out.println("Jumlah Halaman : "+halaman);
-    System.out.println("Sisa Stok : "+stok);
-    System.out.println("Harga : Rp "+harga);
+        System.out.println("Judul : " + judul);
+        System.out.println("Pengarang : " + pengarang);
+        System.out.println("Jumlah Halaman : " + halaman);
+        System.out.println("Sisa Stok : " + stok);
+        System.out.println("Harga : Rp " + harga);
 
     }
 
     void terjual(int jml) {
-            if (stok > 0) {
-                stok -= jml;
-                if (stok < 0) {
-                    stok = 0;
-                }
-            } else {
-                System.out.println("Stok sudah habis.");
+        if (stok > 0) {
+            stok -= jml;
+            if (stok < 0) {
+                stok = 0;
             }
+        } else {
+            System.out.println("Stok sudah habis.");
         }
-        
+    }
+
     void restock(int jml) {
         stok += jml;
     }
 
-    void gantiHarga(int hrg){
-        harga=hrg;
+    void gantiHarga(int hrg) {
+        harga = hrg;
     }
 }
