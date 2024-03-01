2c73996718ff0deb196c699501d103330bab4cd0
adanisalsabilaaa@gmail.com <adanisalsabilaaa@gmail.com>
2024-03-01 11:12:02 +0700

    Jobsheet3_Praktikum3_Soal2_01_1G

--- src/P3/ArrayBalok/Balok.java
+++ src/P3/ArrayBalok/Balok.java
@@ -5,15 +5,27 @@ public class Balok {
     public int lebar;
     public int tinggi;
 
-   
     public Balok(int p, int l, int t) {
         panjang = p;
         lebar = l;
         tinggi = t;
     }
-    
-   
+
     public int hitungVolume() {
         return panjang * lebar * tinggi;
+    }
+}
+
+public class Segitiga {
+    public int alas;
+    public int tinggi;
+
+    public Segitiga(int a, int t) {
+        alas = a;
+        tinggi = t;
+    }
+
+    public double hitungLuas() {
+        return 0.5 * alas * tinggi;
     }
 }
