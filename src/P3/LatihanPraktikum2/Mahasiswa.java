4adcce290df089f137c26e5a9c632dd28f64ff4c
adanisalsabilaaa@gmail.com <adanisalsabilaaa@gmail.com>
2024-03-01 12:37:25 +0700

    Jobsheet3_LatihanPraktikum3_01_1G

--- src/P3/LatihanPraktikum2/Mahasiswa.java
+++ src/P3/LatihanPraktikum2/Mahasiswa.java
@@ -3,6 +3,35 @@ package P3.LatihanPraktikum2;
 import java.util.Scanner;
 
 public class Mahasiswa {
+    public static double hitungRataRata(double[] ipk) {
+        double total = 0;
+        for (double nilai : ipk) {
+            total += nilai;
+        }
+        return total / ipk.length;
+    }
+
+    public static double cariIPKTerbesar(double[] ipk) {
+        double maxIPK = ipk[0];
+        for (double nilai : ipk) {
+            if (nilai > maxIPK) {
+                maxIPK = nilai;
+            }
+        }
+        return maxIPK;
+    }
+
+    public static int cariIndexIPKTerbesar(double[] ipk) {
+        double maxIPK = ipk[0];
+        int indexMaxIPK = 0;
+        for (int i = 1; i < ipk.length; i++) {
+            if (ipk[i] > maxIPK) {
+                maxIPK = ipk[i];
+                indexMaxIPK = i;
+            }
+        }
+        return indexMaxIPK;
+    }
 
     public static void main(String[] args) {
         Scanner sc01 = new Scanner(System.in);
@@ -33,5 +62,15 @@ public class Mahasiswa {
             System.out.println("IPK: " + ipk[i]);
             System.out.println();
         }
+
+        double rataRata = hitungRataRata(ipk);
+        System.out.println("Rata-rata IPK: " + rataRata);
+
+        int indexIPKTerbesar = cariIndexIPKTerbesar(ipk);
+        System.out.println("\nMahasiswa dengan IPK terbesar:");
+        System.out.println("Nama: " + nama[indexIPKTerbesar]);
+        System.out.println("NIM: " + nim[indexIPKTerbesar]);
+        System.out.println("Jenis Kelamin: " + jenisKelamin[indexIPKTerbesar]);
+        System.out.println("IPK: " + ipk[indexIPKTerbesar]);
     }
 }
