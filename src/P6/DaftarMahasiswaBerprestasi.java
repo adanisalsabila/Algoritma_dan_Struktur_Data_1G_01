2ceecd5869f79864045d778696c775c2519881fa
adanisalsabilaaa@gmail.com <adanisalsabilaaa@gmail.com>
2024-03-26 22:56:29 +0700

    Jobsheet5_Praktikum3_Pertanyaan1_1G_01

--- src/P6/DaftarMahasiswaBerprestasi.java
+++ src/P6/DaftarMahasiswaBerprestasi.java
@@ -58,16 +58,15 @@ public class DaftarMahasiswaBerprestasi {
     // method insertionSort()
 
     void insertionSort() {
-        for (int i = 0; i < listMhs.length; i++) {
+        for (int i = 1; i < listMhs.length; i++) {
             Mahasiswa temp = listMhs[i];
             int j = i;
-            while (j > 0 && listMhs[j - 1].ipk > temp.ipk) {
+            while (j > 0 && listMhs[j - 1].ipk < temp.ipk) { 
                 listMhs[j] = listMhs[j - 1];
                 j--;
             }
             listMhs[j] = temp;
-
         }
-
     }
+    
 }
