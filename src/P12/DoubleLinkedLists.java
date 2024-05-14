76336e27151f96f60fdaecd3f6ca44db2de26165
adanisalsabilaaa@gmail.com <adanisalsabilaaa@gmail.com>
2024-05-14 09:36:45 +0700

    Jobsheet10_Praktikum1_1G_01

---
+++ src/P12/DoubleLinkedLists.java
@@ -0,0 +1,83 @@
+package P12;
+
+public class DoubleLinkedLists {
+    Node head;
+    int size;
+
+    public DoubleLinkedLists() {
+        head = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addFirst(int item) {
+        if (isEmpty()) {
+            head = new Node(null, item, null);
+        } else {
+            Node newNode = new Node(null, item, head);
+            head.prev = newNode;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public void addLast(int item) {
+        if (isEmpty()) {
+            addFirst(item);
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            Node newNode = new Node(current, item, null);
+            current.next = newNode;
+            size++;
+        }
+    }
+
+    public void add(int item, int index) throws Exception {
+        if (index < 0 || index > size) {
+            throw new Exception("Nilai indeks di luar batas");
+        } else if (index == 0) {
+            addFirst(item);
+        } else if (index == size) {
+            addLast(item);
+        } else {
+            Node current = head;
+            int i = 0;
+            while (i < index) {
+                current = current.next;
+                i++;
+            }
+            Node newNode = new Node(current.prev, item, current);
+            current.prev.next = newNode;
+            current.prev = newNode;
+            size++;
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void clear() {
+        head = null;
+        size = 0;
+    }
+
+    public void print() {
+        if (!isEmpty()) {
+            Node tmp = head;
+            while (tmp != null) {
+                System.out.print(tmp.data + "\t");
+                tmp = tmp.next;
+            }
+            System.out.println("\nberhasil diisi");
+        } else {
+            System.out.println("Linked Lists Kosong");
+        }
+    }
+}
