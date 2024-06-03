a71fed41cdf79a2b24a75c4fbeb53417f0957736
adanisalsabilaaa@gmail.com <adanisalsabilaaa@gmail.com>
2024-06-03 16:05:53 +0700

    P15_Praktikum1_01_1G

---
+++ src/P15/DoubleLinkedList01.java
@@ -0,0 +1,71 @@
+package P15;
+
+public class DoubleLinkedList01 {
+    Node01 head;
+    int size = 0;
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addFirst(int item, int jarak) {
+        if (isEmpty()) {
+            head = new Node01(null, item, jarak, null);
+        } else {
+            Node01 newNode = new Node01(null, item, jarak, head);
+            head.prev = newNode;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public int getJarak(int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai indeks di luar batas.");
+        }
+        Node01 tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        return tmp.jarak;
+    }
+
+    public int get(int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai indeks di luar batas.");
+        }
+        Node01 tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        return tmp.data;
+    }
+
+    public void remove(int item) {
+        Node01 current = head;
+        while (current != null) {
+            if (current.data == item) {
+                if (current.prev != null) {
+                    current.prev.next = current.next;
+                } else {
+                    head = current.next;
+                }
+                if (current.next != null) {
+                    current.next.prev = current.prev;
+                }
+                size--;
+                break;
+            }
+            current = current.next;
+        }
+    }
+
+    public void clear() {
+        head = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+}
